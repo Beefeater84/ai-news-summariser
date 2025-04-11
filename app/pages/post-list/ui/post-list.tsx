1dc296ddfbb0d521ae36b5dbcbf06ad3e0ec16bf
Miaseodov Tony <myasoedov.blog@gmail.com>
2025-04-11 08:34:14 +0300

    list article styles

--- app/pages/post-list/ui/post-list.tsx
+++ app/pages/post-list/ui/post-list.tsx
@@ -1,9 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { ArticleCard } from "@/widgets/article-card";
-import type { IArticleCard } from "@/entities/post";
-import { useEffect, useState } from "react";
 import { listPosts } from "@/entities/post";
 import styles from "./post-list.module.css";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { Skeleton } from "./skeleton";
 
 export const PostList = () => {
@@ -15,12 +13,11 @@ export const PostList = () => {
 
   return (
     <section className="container">
-      <h1>List with transcripted video</h1>
-      <section className={styles.list}>
+      <div className={styles.list}>
         {isLoading && <Skeleton />}
         {!isLoading &&
           posts.map((article) => <ArticleCard key={article.id} {...article} />)}
-      </section>
+      </div>
     </section>
   );
 };
