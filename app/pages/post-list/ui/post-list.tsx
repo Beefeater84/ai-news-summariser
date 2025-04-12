8f601600f763d51899c8f3458e181c7fe28309a7
Miaseodov Tony <myasoedov.blog@gmail.com>
2025-04-12 15:03:43 +0300

    test

--- app/pages/post-list/ui/post-list.tsx
+++ app/pages/post-list/ui/post-list.tsx
@@ -5,19 +5,20 @@ import styles from "./post-list.module.css";
 import { Skeleton } from "./skeleton";
 
 export const PostList = () => {
-  const {
-    data: posts = [],
-    isLoading,
-    isError,
-  } = useQuery({ queryKey: ["post-list"], queryFn: listPosts });
+  // const {
+  //   data: posts = [],
+  //   isLoading,
+  //   isError,
+  // } = useQuery({ queryKey: ["post-list"], queryFn: listPosts });
 
   return (
     <section className="container">
-      <div className={styles.list}>
+      <h1>TEST</h1>
+      {/* <div className={styles.list}>
         {isLoading && <Skeleton />}
         {!isLoading &&
           posts.map((article) => <ArticleCard key={article.id} {...article} />)}
-      </div>
+      </div> */}
     </section>
   );
 };
