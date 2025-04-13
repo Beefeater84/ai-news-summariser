615d2ad0f01651c453fd1aa711bffcbf4cee0422
Miaseodov Tony <myasoedov.blog@gmail.com>
2025-04-13 15:37:39 +0300

    +

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  route("*?", "./pages/post-list/index.ts"),
-  // index("./pages/post-list/index.ts"),
+  // route("*?", "./pages/post-list/index.ts"),
+  index("./pages/post-list/index.ts"),
   route("article/:articleSlug", "./pages/article/index.ts"),
 ] satisfies RouteConfig;
